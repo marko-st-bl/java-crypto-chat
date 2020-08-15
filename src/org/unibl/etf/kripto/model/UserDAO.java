cd2d31ba52d378a5e8397778d4152c0dcfb86511
Marko Stojanovic <st.marko.bl@gmail.com>
2020-08-15 14:33:28 +0200

    Changed read/write users function to use encrypted file instead of plaintext

--- src/org/unibl/etf/kripto/model/UserDAO.java
+++ src/org/unibl/etf/kripto/model/UserDAO.java
@@ -1,18 +1,18 @@
 package org.unibl.etf.kripto.model;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.Properties;
 
+import javax.swing.JOptionPane;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.unibl.etf.kripto.util.BouncyCastleCrypto;
+import org.unibl.etf.kripto.util.Hash;
 import org.unibl.etf.kripto.util.PasswordUtil;
 import org.unibl.etf.kripto.util.PropertiesUtil;
 
@@ -35,7 +35,12 @@ public class UserDAO {
 		}
 		return retVal;
 	}
-
+	
+	/*
+	 * This function is not used by app, its meant to crate file with users.
+	 * In order to use to funtion you must provide rootCA private key and cert
+	 * in private and certs directories.
+	 */
 	public static void writeUsersToFile() throws Exception {
 		User admin = new User("admin", "admin");
 		User marko = new User("marko", "marko");
@@ -43,39 +48,51 @@ public class UserDAO {
 		User test = new User("test", "test");
 		User date = new User("date", "date");
 		User hacker = new User("hacker", "hacker");
+		
+		String output="";
+		output += admin + "\n";
+		output += marko + "\n";
+		output += jovana + "\n";
+		output += test + "\n";
+		output += date + "\n";
+		output += hacker + "\n";
 
-		File users = new File(PROPS.getProperty("users.path"));
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(users), StandardCharsets.UTF_8)));
-		pw.println(admin);
-		pw.println(marko);
-		pw.println(jovana);
-		pw.println(test);
-		pw.println(date);
-		pw.println(hacker);
-		pw.close();
+		String key = PROPS.getProperty("users.pass");
+		Cert root = new Cert("rootCA");
+		byte [] signed = BouncyCastleCrypto.signData(output.getBytes(), root.getCertificate(), 
+				root.getPrivateKey(), Hash.SHA256.toString());
+		byte [] enc = BouncyCastleCrypto.aesecbEncrypt(BouncyCastleCrypto.defineKeyForAES(key.getBytes()), signed);
+		Path path=Paths.get(PROPS.getProperty("users.path"));
+		Files.write(path, enc);
 
 	}
 
 	public static void readUsersFromFile() {
-		BufferedReader in = null;
-		String line = "";
+		Security.addProvider(new BouncyCastleProvider());
+		Path path=Paths.get(PROPS.getProperty("users.path"));
+		String key = PROPS.getProperty("users.pass");
 		try {
-			in = new BufferedReader(new InputStreamReader(new FileInputStream(PROPS.getProperty("users.path")), StandardCharsets.UTF_8));
-			while((line = in.readLine()) != null) {
-				String userData[] = line.split(":");
-				users.add(new User(userData[0], userData[1], userData[2]));
+			byte[] enc = Files.readAllBytes(path);
+			byte[] dec = BouncyCastleCrypto.aesecbDecryption(BouncyCastleCrypto.defineKeyForAES(key.getBytes()), enc);
+			if(BouncyCastleCrypto.verifSignData(dec)) {
+				String usersData = new String(BouncyCastleCrypto.parseSignedData(dec));
+				String[] lines = usersData.split("\n");
+				for(int i=0; i< lines.length; i++) {
+					String[] line=lines[i].split(":");
+					users.add(new User(line[0], line[1], line[2]));
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Unauthorized change of users file.", "Invalid file!", JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
 	}
 
 }
